a6de4fff3b5c66872d01a01f80426db72518eafd
digios <digios.till@yandex.ru>
2019-04-10 01:07:27 +0700

    Tmp

--- TelnetTemp/Functions.cs
+++ TelnetTemp/Functions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Win32;
+﻿using System.Net;
+using System.Text;
+using Microsoft.Win32;
 
 namespace TelnetTemp
 {
@@ -37,6 +39,40 @@ namespace TelnetTemp
             catch { }
 
             return false;
+        }
+
+        public static string PostMethod(string data)
+        {
+            //создаем ссылку на которую будем слать наш запрос
+            //string postUrl = "http://arsenal.army/index.php?option=com_jshopping&controller=modem&task=smsGet";
+
+            byte[] sentData = Encoding.UTF8.GetBytes("json=" + System.Web.HttpUtility.UrlEncode(data, Encoding.UTF8));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Settings.Instance.UrlHost);
+            request.Method = "POST";
+            request.SendChunked = true;
+
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            request.ContentLength = sentData.Length;
+
+            //начинаем передачу
+            using (var newStream = request.GetRequestStream())
+            {
+                newStream.Write(sentData, 0, sentData.Length);
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();//ждем ответа от сервера
+            /*string responseToString = "";
+
+            //если ответ пришел и он не нулевой
+            if (response != null)
+            {
+                var strreader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);//получаем ответ в кодировке утф8
+                responseToString = strreader.ReadToEnd();
+            }
+            return responseToString; //возвращаем строку с ответом*/
+            return null;
         }
     }
 }
